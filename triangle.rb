d49a174779ad9cab5c24329779274ea32b98b8ff
Paulo Cuambe <pamossecuambe@gmail.com>
2022-04-26 12:42:42 +0200

    solve triangle project exercise

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,13 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == b and b == c
+    :equilateral
+  elsif a == b or b == c or a == c
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
